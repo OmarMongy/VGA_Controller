f4bffecbf3df133c66b9f9b236a870d974657347
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-09-12 03:22:52 +0300

    Create vertical_counter.v

---
+++ src/vertical_counter.v
@@ -0,0 +1,25 @@
+module vertical_counter(
+input clk, reset_n, enable,
+output [9:0] pixel_y,
+output done_y
+);
+reg [9:0] pixel_reg, pixel_next;
+
+always@(posedge clk, negedge reset_n)
+begin
+  if(~reset_n)
+   pixel_reg <= 10'b0;
+  else if(enable)
+   pixel_reg <= pixel_next;
+  else
+    pixel_reg <= pixel_reg; 
+end
+
+always@(*)
+begin
+  pixel_next = done_y? 10'b0 : pixel_reg + 1;  
+end
+
+assign pixel_y = pixel_reg;
+assign done_y = pixel_reg == 10'd524;
+endmodule
